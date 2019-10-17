0eefb05baa860efcb5e8d4286da93ebef5453034
Konstantin <kdobr77@gmail.com>
2019-10-17 23:15:44 +0300

    Add files via upload

---
+++ AgeSorterApp.java
@@ -0,0 +1,69 @@
+import java.io.BufferedInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class AgeSorterApp {
+
+	public static void main(String[] args) throws IOException {
+		FileOutputStream outputStream = new FileOutputStream("testFile");
+		int[] test = { 15, 24, 3, 45, 15, 45, 15 };
+		for (int i = 0; i < test.length; i++) {
+			outputStream.write(test[i]);
+		}
+		outputStream.close();
+		ageSorter("testFile", "test2");
+
+	}
+	
+	public static void ageSorter(String pathNameIn, String pathNameOut) {
+		int[] age = new int[127]; // array for age. size = 127. think it's enough
+		int[] qty = new int[127]; // array for quantity (qty) of people same age
+
+		try (BufferedInputStream br = new BufferedInputStream(new FileInputStream(new File(pathNameIn)));
+				DataOutputStream brOut = new DataOutputStream(new FileOutputStream(new File(pathNameOut)))) {
+
+			// read data from file and sent to stream
+			IntStream.generate(() -> {
+				try {
+					return br.read();
+				} catch (IOException e) {
+					return 0;
+				}
+			}).limit(br.available())
+					// sorting data to arrays of age and qty
+					.forEach(n -> {
+						for (int i = 0; i < age.length; i++) {
+							if (age[i] == n) {
+								qty[i] += 1;
+								break;
+							} else {
+								if (age[i] == 0) {
+									age[i] = n;
+									qty[i] = 1;
+									break;
+								}
+							}
+						}
+					});
+			// uniting data from two arrays and and writing it to file
+			for (int i = 0; i < age.length; i++) {
+				if (age[i] != 0) {
+					System.out.println("age :" + age[i] + " qty: " + qty[i]);
+					int temp = (age[i] << 16) | qty[i];
+					System.out.println("temp " + temp);
+					try {
+						brOut.writeInt(temp);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
